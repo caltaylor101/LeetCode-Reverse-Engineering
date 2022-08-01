a56d944aed82b0c83dc3d9e0fe8050725a155602
caltaylor <Caltaylor101@gmail.com>
2022-08-01 13:13:57 -0600

    Count And Say comments and recursive approach.

--- LeetCode Reverse Engineering/ReverseEngineeringPart3.cs
+++ LeetCode Reverse Engineering/ReverseEngineeringPart3.cs
@@ -36,32 +36,68 @@ namespace LeetCode_Reverse_Engineering
             return new string(string.Join("", say));
         }*/
 
-        private List<string> say = new List<string>() { "1" };
-        private List<string> newSay = new List<string>() { };
-        public string CountAndSay(int n)
+        
+        public string CountAndSay2(int n)
         {
+            //Initialize the base case.
+            string say = "1";
+            //A way to build strings.
+            StringBuilder newSay = new StringBuilder();
+
+            //We already have the base case set, so we want anything greater than 1.
             while (n > 1)
             {
+                //Reset the counter every loop.
                 int counter = 1;
-                for (int i = 0; i < say.Count; i++)
+                //Loop through the string.
+                for (int i = 0; i < say.Length; i++)
                 {
-                    if(i+1 < say.Count && say[i] == say[i+1])
-                    {
-                        counter++;
-                    }
+                    //Check to see if the next number is the same, if it is increase the counter and continue. 
+                    if (i + 1 < say.Length && say[i] == say[i + 1]) counter++;
+                    //Otherwise, we can add our counter and the current element. 
+                    //Reset the counter and start again.
                     else
                     {
-                        newSay.Add(counter.ToString());
-                        newSay.Add(say[i]);
+                        newSay.Append(counter.ToString() + say[i]);
                         counter = 1;
                     }
                 }
-                say = new List<string>(newSay);
+                //Update say to the new string.
+                say = newSay.ToString();
+                //Clear the string builder.
                 newSay.Clear();
+                //Lower the iteration account.
                 n--;
             }
+            //Return the string. 
+            return say;
+        }
 
-            return new string(string.Join("", say));
+        public string CountAndSay(int n)
+        {
+            //This is our base case and where the recurse functions will start.
+            if (n == 1) return "1";
+            //This will call multiple functions till it reaches the base case. 
+            string say = CountAndSay(n - 1);
+            //Create a stringBuilder to return.
+            StringBuilder newSay = new StringBuilder();
+            int count = 1;
+
+            //Basically the same logic as the iterative approach.
+            for (int i = 0; i < say.Length; i++)
+            {
+                if (i + 1 < say.Length && say[i] == say[i + 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    newSay.Append(count.ToString() + say[i]);
+                    count = 1;
+                }
+            }
+            //return the new string we created. 
+            return newSay.ToString();
         }
 
 
